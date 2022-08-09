20e1e10b22524948f40f71d68d6a2eb7bbe12f11
Eяw!и Gontier <ierwinx@me.com>
2022-08-09 10:28:15 -0500

    Initial Commit

---
+++ CicloVida/CicloVidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CicloVidaApp.swift
+//  CicloVida
+//
+//  Created by Erwin Luz León on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CicloVidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
